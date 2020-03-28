55b2764c62ce9d33492d7c153ae9a6d6509caad0
moumen-soliman <moumensoliman@gmail.com>
2020-03-28 20:07:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = [];
 
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +14,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8000;
+
+app.listen(port, () => console.log(`Server running! Running on localhost: ${port}!`));
+
+app.get('/all', (req, res) => {
+    res.send(projectData)
+})
+
+app.post('/add', callBack);
+function callBack(req, res) {
+  projectData.push(req.body);
+  res.send(true);
+}
